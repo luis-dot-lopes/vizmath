5cc2af9743618116447915c58aa30115232c423f
luis-dot-lopes <luisantonio20054@gmail.com>
2024-05-17 21:48:47 -0300

    Made bezier visualization more dinamic

--- bezier.c
+++ bezier.c
@@ -4,7 +4,7 @@
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 
-#define POINT_RADIUS 10
+#define POINT_RADIUS 8
 
 int
 main(void)
@@ -13,26 +13,45 @@ main(void)
 
   SetTargetFPS(60);
 
-  Vector2 p0 = { .x = 200, .y = 200 }, p1 = { .x = 300, .y = 100 },
-          p2 = { .x = 400, .y = 200 };
+  Vector2 nodes[] = { { .x = 200, .y = 200 },
+                      { .x = 300, .y = 100 },
+                      { .x = 400, .y = 200 } };
+  int node_count = sizeof(nodes) / sizeof(nodes[0]);
+  int selected_node = node_count;
 
   while (!WindowShouldClose()) {
+
+    Vector2 mouse = GetMousePosition();
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+      for (int i = 0; i < node_count; ++i) {
+        if (CheckCollisionPointCircle(mouse, nodes[i], POINT_RADIUS)) {
+          selected_node = i;
+          break;
+        }
+      }
+    } else if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+      selected_node = node_count;
+    }
+
+    if (selected_node < node_count) {
+      nodes[selected_node] = mouse;
+    }
+
     BeginDrawing();
     {
       ClearBackground(BLACK);
 
-      DrawLineV(p0, p1, WHITE);
-      DrawLineV(p1, p2, WHITE);
+      DrawLineV(nodes[0], nodes[1], WHITE);
+      DrawLineV(nodes[1], nodes[2], WHITE);
 
-      DrawCircleV(p0, POINT_RADIUS, WHITE);
-      DrawCircleV(p1, POINT_RADIUS, WHITE);
-      DrawCircleV(p2, POINT_RADIUS, WHITE);
+      DrawCircleV(nodes[0], POINT_RADIUS, WHITE);
+      DrawCircleV(nodes[1], POINT_RADIUS, WHITE);
+      DrawCircleV(nodes[2], POINT_RADIUS, WHITE);
 
-      // p = (p0 -2p1 + p2)t^2 + (p1 - p2)2t + p2
-      Vector2 p = p0;
+      Vector2 p = nodes[0];
       for (float t = 0.0f; t <= 1.0f; t += 0.05f) {
-        Vector2 p3 = Vector2Lerp(p0, p1, t);
-        Vector2 p4 = Vector2Lerp(p1, p2, t);
+        Vector2 p3 = Vector2Lerp(nodes[0], nodes[1], t);
+        Vector2 p4 = Vector2Lerp(nodes[1], nodes[2], t);
         Vector2 p5 = Vector2Lerp(p3, p4, t);
         DrawLineV(p, p5, GREEN);
         DrawCircleV(p5, 2, RED);
