3eec0a1d0608a94db8761e2106a27e2135c0537d
luis-dot-lopes <luisantonio20054@gmail.com>
2024-05-18 09:55:48 -0300

    Improved bezier visualization and updated README

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This project aims to produce visualizations for
 various math concepts. Currently there are
 visualizations for newton's method for finding roots
 of polynomial equations, inspired by 3Blue1Brown's
-video on the subject, and for the Mandelbrot set,
-that is a classic math graph. This project uses
+video on the subject, for the Mandelbrot set,
+that is a classic math graph, and for bezier curves,
+that are used extensively in vector graphics. This project uses
 [Raylib](https://www.raylib.com/) for its graphics.
--- bezier.c
+++ bezier.c
@@ -5,6 +5,7 @@
 #define SCREEN_HEIGHT 600
 
 #define POINT_RADIUS 8
+#define NODE_CAP 10
 
 int
 main(void)
@@ -13,12 +14,15 @@ main(void)
 
   SetTargetFPS(60);
 
-  Vector2 nodes[] = { { .x = 200, .y = 200 },
-                      { .x = 250, .y = 100 },
-                      { .x = 350, .y = 100 },
-                      { .x = 400, .y = 200 } };
-  int node_count = sizeof(nodes) / sizeof(nodes[0]);
+  Vector2 nodes[NODE_CAP] = { { .x = 200, .y = 200 },
+                              { .x = 250, .y = 100 },
+                              { .x = 350, .y = 100 },
+                              { .x = 400, .y = 200 } };
+  int node_count = 4;
   int selected_node = node_count;
+  int total_steps = 40;
+  int steps = total_steps;
+  float step_size = 1.0f / total_steps;
 
   while (!WindowShouldClose()) {
 
@@ -32,6 +36,16 @@ main(void)
       }
     } else if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
       selected_node = node_count;
+    }
+
+    if (IsKeyPressed(KEY_N) && steps < total_steps) {
+      steps++;
+    } else if (IsKeyPressed(KEY_P) && steps > 0) {
+      steps--;
+    } else if (IsKeyPressed(KEY_A) && node_count < NODE_CAP) {
+      nodes[node_count++] = mouse;
+    } else if (IsKeyPressed(KEY_R) && node_count > 0) {
+      node_count--;
     }
 
     if (selected_node < node_count) {
@@ -50,7 +64,8 @@ main(void)
       }
 
       Vector2 p = nodes[0];
-      for (float t = 0.0f; t <= 1.0f; t += 0.05f) {
+      for (int k = 0; k <= steps; ++k) {
+        float t = k * step_size;
         Vector2 lerp_nodes[node_count];
         int lerp_count = node_count;
         for (int i = 0; i < node_count; ++i) {
@@ -59,8 +74,15 @@ main(void)
         while (lerp_count > 1) {
           for (int j = 0; j < lerp_count - 1; ++j) {
             lerp_nodes[j] = Vector2Lerp(lerp_nodes[j], lerp_nodes[j + 1], t);
+            if (k == steps)
+              DrawCircleV(lerp_nodes[j], 3, RED);
           }
           lerp_count--;
+          if (k == steps) {
+            for (int j = 0; j < lerp_count - 1; ++j) {
+              DrawLineV(lerp_nodes[j], lerp_nodes[j + 1], BLUE);
+            }
+          }
         }
         DrawLineV(p, lerp_nodes[0], GREEN);
         DrawCircleV(lerp_nodes[0], 2, RED);
