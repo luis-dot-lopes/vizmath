cf01cb5d1b86fb32156e9cfc17f8dcbc4030f229
luis-dot-lopes <luisantonio20054@gmail.com>
2024-05-21 10:10:51 -0300

    Created a decent bezier curve visualization on fragment shader using newton's method

--- bezier.fs
+++ bezier.fs
@@ -27,8 +27,30 @@ vec2 newton_bezier(vec2 t, vec2 p) {
 	return t;
 }
 
+// this function uses newton's method to calculate the distance
+// between the point p and the bezier curve
+float minimum_bezier(float t, vec2 p) {
+	for(int i = 0; i < 10; ++i) {
+		float it = 1.0 - t;
+		vec2 b = it * it * p0 + 2 * it * t * p1 + t * t * p2;
+		vec2 db = 2*it*(p1 - p0) + 2*t*(p2 - p1);
+		vec2 ddb = 2*(p2 - 2 * p1 + p0);
+		float df = 2*dot(db, b - p);
+		float ddf = 2*(dot(b - p, ddb) + dot(db, db));
+		t -= df/ddf;
+	}
+	return t;
+}
+
 void main() {
 	vec2 z = vec2(fragTexCoord.x, fragTexCoord.y * 0.75);
+	float t = minimum_bezier(0.5, z);
+	float it = 1.0 - t;
+	vec2 b = it * it * p0 + 2 * it * t * p1 + t * t * p2;
+	if(0 <= t && t <= 1 && length(b - z) < 0.002) {
+		fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+		return;
+	}
 	if(length(p0 - z) < 1e-2 || length(p1 - z) < 1e-2 || length(p2 - z) < 1e-2) {
 		fragColor = vec4(1.0);
 		return;
@@ -52,6 +74,7 @@ void main() {
 		return;
 	}
 	*/
+	/*
 	vec2 t0 = vec2((clamp(z.x, p0.x, p2.x) - p0.x) / (p2.x - p0.x));
 	vec2 t = newton_bezier(t0, z);
 	float it = 1.0 - t.x;
@@ -60,5 +83,6 @@ void main() {
 		fragColor = vec4(1.0, 0.0, 0.0, 1.0);
 		return;
 	}
+	*/
 	fragColor = vec4(0.5);
 }
